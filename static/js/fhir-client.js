69bce09458ef1ea37b7f0b2b59a295fe17094845
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2013-11-16 01:24:15 -0500

    Updated fhir js lib

--- static/js/fhir-client.js
+++ static/js/fhir-client.js
@@ -9059,8 +9059,8 @@ function Search(p) {
     return function(data, status) {
 
       nextPageUrl = null; 
-      if(data.link) {
-        var next = data.link.filter(function(l){
+      if(data.feed.link) {
+        var next = data.feed.link.filter(function(l){
           return l.rel === "next";
         });
         if (next.length === 1) {
